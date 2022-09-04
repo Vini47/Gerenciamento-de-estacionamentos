cb136fb344652f788fffebc54a3e269f2efdc628
Vini47 <79549264+Vini47@users.noreply.github.com>
2022-09-04 08:42:47 -0300

    Add files via upload

---
+++ TP2/src/models/Estacionamento.java
@@ -0,0 +1,35 @@
+package models;
+
+import java.time.LocalTime;
+
+public class Estacionamento {
+
+//    LocalTime horaAbertura = LocalTime.of();
+//    LocalTime horaFechamento = LocalTime.of();
+    String horaAbertura;
+    String horaFechamento;
+    int capacidade;
+
+
+    
+
+    public void setHoraAbertura(String horaAbertura) {
+        this.horaAbertura = horaAbertura;
+    }
+
+    public String getHoraFechamento() {
+        return horaFechamento;
+    }
+
+    public void setHoraFechamento(String horaFechamento) {
+        this.horaFechamento = horaFechamento;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+}
