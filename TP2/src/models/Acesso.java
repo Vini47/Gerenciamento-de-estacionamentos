3a68eb10de99ffc3cd3ef15adb53541644213371
obobinho <80782792+obobinho@users.noreply.github.com>
2022-09-05 20:11:06 -0300

    Update Acesso.java

--- TP2/src/models/Acesso.java
+++ TP2/src/models/Acesso.java
@@ -1,4 +1,61 @@
 package models;
 
+import java.time.*;
+import java.util.Date;
+
 public class Acesso {
+	protected Date dataEntrada;
+	protected Date dataSaida;
+	protected Date horaEntrada;
+	protected Date horaSaida;
+	private Veiculo veiculo;
+	
+	public Acesso(Date dataEntrada, Date dataSaida, Date horaEntrada, Date horaSaida, Veiculo veiculo) {
+		this.dataEntrada = dataEntrada;
+		this.dataSaida = dataSaida;
+		this.horaEntrada = horaEntrada;
+		this.horaSaida = horaSaida;
+		this.veiculo = veiculo;
+	}
+
+	public Date getDataEntrada() {
+		return dataEntrada;
+	}
+
+	public void setDataEntrada(Date dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+
+	public Date getDataSaida() {
+		return dataSaida;
+	}
+
+	public void setDataSaida(Date dataSaida) {
+		this.dataSaida = dataSaida;
+	}
+
+	public Date getHoraEntrada() {
+		return horaEntrada;
+	}
+
+	public void setHoraEntrada(Date horaEntrada) {
+		this.horaEntrada = horaEntrada;
+	}
+
+	public Date getHoraSaida() {
+		return horaSaida;
+	}
+
+	public void setHoraSaida(Date horaSaida) {
+		this.horaSaida = horaSaida;
+	}
+
+	public Veiculo getVeiculo() {
+		return veiculo;
+	}
+
+	public void setVeiculo(Veiculo veiculo) {
+		this.veiculo = veiculo;
+	}
 }
+
